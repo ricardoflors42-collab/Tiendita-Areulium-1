ab3fa13e5555c630565e54de0ad033ba61b34e89
Andres <128873846+Andres-Guzman-19@users.noreply.github.com>
2025-10-22 21:28:02 -0500

    Enhance menu.py with project details and structure
    Updated project documentation and added details about the retail store Aurelion, including team members, problem statement, solution, dataset structure, and pseudocode for the menu system.

--- menu.py
+++ menu.py
@@ -13,20 +13,61 @@ Seleccione alguna opcion:
 5. Salir
 """
 integrantes = """
-Andres Guzman
+# Integrantes
+1. Ricardo Flores ven
+2. Andrés Guzmán col
+3. 
 """
 
 tema = """
 TEMA:
-PROBLLEMA:
+Tienda retail Aurelion
+PROBLEMA:
+Planificación deficiente y nula segmentación de clientes en Tienda Aurelion.
+
+La tienda opera sin una planificación de ventas estructurada, lo que desencadena una gestión de inventario imprecisa. De forma paralela, su enfoque comercial es indiferenciado, tratando a todos los clientes por igual. Esta falta de segmentación impide diseñar estrategias de marketing eficientes y personalizadas que realmente conecten con los consumidores e impulsen la demanda.
 SOLUCIÓN:
+1. Normalizar la bases de datos.
+2. Identificar y definir las variables clave necesarias para alimentar los modelo
+3. Desarrollar modelos de Machine Learning para la segmentacion automatica de clientes y la prediccion de la demanda
+4. Implementar un Dashboard para visualizar y monitorear los KPI de la tienda
+
 """
 
 datos = """
-TABLA PRODUCTOS
-TABLA DETALLE_VENTAS
-TABLA VENTAS
-TABLA CLIENTES
+Productos
+Atributo        | Tipo de dato | Escala de medición | Identificadores
+id_producto     | Int          | intervalo          | PK
+nombre_producto | strings      | nominal            | 
+categoria       | strings      | nominal            |
+precio_unitario | float        | razón              |
+
+Clientes
+Atributo       | Tipo de dato | Escala de medición | Identificadores
+id_cliente     | int          | intervalo          | PK
+nombre_cliente | strings      | nominal            | 
+email          | strings      | nominal            | 
+ciudad         | strings      | nominal            |
+fecha_alta     | date         | intervalo          |
+
+ Ventas
+Atributo         | Tipo de dato | Escala de medición | Identificadores
+id_venta         | int          | intervalo          | PK
+fecha	         | date         | intervalo          |
+id_cliente	     | int          | intervalo          | FK 
+nombre_cliente	 | strings      | nominal            | 
+apellido_cliente | strings      | nominal            | 
+email            | strings      | nominal            |
+medio_pago       | strings      | nominal            |
+
+ Detalle_ventas
+Atributo        | Tipo de dato | Escala de medición | Identificadores
+id_venta        | int          | intervalo          | FK
+id_producto     | int          | intervalo          | FK
+nombre_producto | string       | nominal            |
+cantidad        | int          | razón              |
+precio_unitario | float        | razón              |
+importe         | float        | razón              |
 """
 
 pseudocodigo = """
@@ -59,3 +100,4 @@ while menu != 5:
             print('Gracias!')
         case _:
             print('Seleccione una opción valida')
+
